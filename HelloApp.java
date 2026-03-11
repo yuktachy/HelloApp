c6fbb012e5c82860a259a2414e40d85348a81dbd
yuktachy <yuktachoudhary22@gmail.com>
2026-03-11 11:16:12 +0530

    coded UC3 to display default name

--- HelloApp.java
+++ HelloApp.java
@@ -1,6 +1,9 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name = args[0];
+        String name = "World";
+        if (args.length > 0) {
+            name = args[0];
+        }
         System.out.println("Hello, " + name + "!");
     }
 }
